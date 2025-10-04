ca7868dc808873f009536b764ac852dba04175b4
AnotherGosu <anothergosu@gmail.com>
2025-10-04 16:09:04 +0900

    Updated pagination

--- src/components/common/Pagination/PaginationList.tsx
+++ src/components/common/Pagination/PaginationList.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronsLeftIcon,
+  ChevronsRightIcon,
+} from "lucide-react";
 import { usePathname, useSearchParams } from "next/navigation";
 
 import { cn } from "@/utils/cn";
@@ -15,24 +21,70 @@ export const PaginationList = ({ totalPages }: PaginationListProps) => {
     usePaginationList(totalPages);
 
   return (
-    <ul className="flex flex-wrap gap-2">
-      {pages.map((page) => {
-        const isDisabled = currentPage === page;
+    <ul className="flex justify-center gap-4">
+      <div className="flex gap-1">
+        <ButtonLink
+          href={getPaginationLink(1)}
+          aria-label="Go to first page"
+          className={cn("size-8 p-0", {
+            "pointer-events-none opacity-50": currentPage === 1,
+          })}
+        >
+          <ChevronsLeftIcon />
+        </ButtonLink>
 
-        return (
-          <li key={page}>
-            <ButtonLink
-              href={getPaginationLink(page)}
-              aria-label={`Go to page ${page}`}
-              className={cn("size-10 p-0", {
-                "pointer-events-none opacity-50": isDisabled,
-              })}
-            >
-              {page}
-            </ButtonLink>
-          </li>
-        );
-      })}
+        <ButtonLink
+          href={getPaginationLink(currentPage - 1)}
+          aria-label="Go to previous page"
+          className={cn("size-8 p-0", {
+            "pointer-events-none opacity-50": currentPage === 1,
+          })}
+        >
+          <ChevronLeftIcon />
+        </ButtonLink>
+      </div>
+
+      <div className="flex gap-1">
+        {pages.map((page) => {
+          const isDisabled = currentPage === page;
+
+          return (
+            <li key={page}>
+              <ButtonLink
+                href={getPaginationLink(page)}
+                aria-label={`Go to page ${page}`}
+                className={cn("size-8 p-0", {
+                  "pointer-events-none opacity-50": isDisabled,
+                })}
+              >
+                {page}
+              </ButtonLink>
+            </li>
+          );
+        })}
+      </div>
+
+      <div className="flex gap-1">
+        <ButtonLink
+          href={getPaginationLink(currentPage + 1)}
+          aria-label="Go to next page"
+          className={cn("size-8 p-0", {
+            "pointer-events-none opacity-50": currentPage === totalPages,
+          })}
+        >
+          <ChevronRightIcon />
+        </ButtonLink>
+
+        <ButtonLink
+          href={getPaginationLink(totalPages)}
+          aria-label="Go to last page"
+          className={cn("size-8 p-0", {
+            "pointer-events-none opacity-50": currentPage === totalPages,
+          })}
+        >
+          <ChevronsRightIcon />
+        </ButtonLink>
+      </div>
     </ul>
   );
 };
@@ -43,7 +95,12 @@ const usePaginationList = (totalPages: number) => {
 
   const currentPage = Number(searchParams.get("page") || 1);
 
-  const pages = [...Array(totalPages).keys()].map((key) => key + 1);
+  const start = currentPage < 4 ? 0 : currentPage - 3;
+  const end = currentPage < 4 ? 5 : currentPage + 2;
+
+  const pages = [...Array(totalPages).keys()]
+    .map((key) => key + 1)
+    .slice(start, end);
 
   const getPaginationLink = (page: number) => {
     const params = new URLSearchParams(searchParams.toString());
